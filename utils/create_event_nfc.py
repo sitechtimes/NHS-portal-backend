df5f28cc23dd51ecccd37b231bf54b21cfad9ea9
Samuel Kipnis <sam.kipnis@gmail.com>
2025-09-15 08:03:13 -0700

    .

--- utils/create_event_nfc.py
+++ utils/create_event_nfc.py
@@ -6,10 +6,9 @@ load_dotenv()
 
 
 def create_event_nfc(name, time_start, time_end):
-    url = f"{os.getenv(
-    "NFC_BACKEND_URL",
-)
-}/events/create/"
+    key = login_nfc()
+    print(f"Login key: {key}")
+    url = f"http://localhost:9000/events/create/"
     payload = {
         "name": name,
         "timeStart": time_start,
@@ -17,10 +16,23 @@ def create_event_nfc(name, time_start, time_end):
         "restricted": False,
         "type": "NHS",
     }
-    response = requests.post(url, json=payload, timeout=10)
+    headers = {"Authorization": f"Bearer {key}"}
+    response = requests.post(url, json=payload, headers=headers, timeout=10)
     if response.status_code == 201:
         return response.json()
     else:
         return {
             "error": f"Failed to create event: {response.status_code}, {response.text}"
         }
+
+
+def login_nfc():
+    # url = f"{os.getenv('NFC_BACKEND_URL')}/auth/login/"
+    url = f"http://localhost:9000/auth/login/"
+    payload = {
+        "username": os.getenv("NFC_BACKEND_USERNAME"),
+        "password": os.getenv("NFC_BACKEND_PASSWORD"),
+    }
+    response = requests.post(url, json=payload, timeout=10)
+    print(response.json().get("key"))
+    return response.json().get("key")
