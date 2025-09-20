574d9493f2ef61fa4f98a03039d511cc9316114f
Samuel Kipnis <sam.kipnis@gmail.com>
2025-09-20 17:00:59 -0400

    register models for admin panel.

--- guidance/admin.py
+++ guidance/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import (
+    BiographicalQuestion,
+    BiographicalQuestionInstance,
+    Recommendation,
+    Announcement,
+)
 
-# Register your models here.
+admin.site.register(BiographicalQuestion)
+admin.site.register(BiographicalQuestionInstance)
+admin.site.register(Recommendation)
+admin.site.register(Announcement)
--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import (
+    ServiceProfile,
+    LeadershipProfile,
+    PersonalProfile,
+    ServiceActivity,
+    LeadershipActivity,
+    EventActivity,
+    GPARecord,
+)
 
-# Register your models here.
+admin.site.register(ServiceProfile)
+admin.site.register(LeadershipProfile)
+admin.site.register(PersonalProfile)
+admin.site.register(ServiceActivity)
+admin.site.register(LeadershipActivity)
+admin.site.register(EventActivity)
+admin.site.register(GPARecord)
