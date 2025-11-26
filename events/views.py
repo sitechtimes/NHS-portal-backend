48ae98b93ed89f4baf26e3958bbc988925ef891d
Samuel Kipnis <sam.kipnis@gmail.com>
2025-11-26 11:08:11 -0500

    .

--- events/views.py
+++ events/views.py
@@ -1,15 +1,17 @@
 from rest_framework import mixins, viewsets
-from backend.permissions import IsTeacher, IsGuidance, IsAdmin
+from backend.permissions import IsStudent, IsTeacher, IsGuidance, IsAdmin
 from .models import ServiceEvent
 from .serializers import EventSerializer
 
 
-class EventViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+class EventViewSet(viewsets.ModelViewSet):
     queryset = ServiceEvent.objects.all()
     serializer_class = EventSerializer
 
     def get_permissions(self):
-        if self.action == "create":
+        if self.action in ("retrieve", "list"):
+            perms = [IsStudent | IsTeacher | IsGuidance | IsAdmin]
+        elif self.action == "create":
             perms = [IsTeacher | IsGuidance | IsAdmin]
         else:
             perms = [IsGuidance | IsAdmin]
